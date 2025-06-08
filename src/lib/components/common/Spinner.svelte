24050a5d41a7c16ed840127d1a6b562547e7224c
szalovszky <david@szalovszky.com>
2025-06-09 00:49:49 +0200

    feat(style): add spinner svg

--- src/lib/components/common/Spinner.svelte
+++ src/lib/components/common/Spinner.svelte
@@ -1,19 +1,12 @@
-<script lang="ts">
-  export let small: boolean = true;
-</script>
-
 <div
-  class="block h-6 w-6 animate-[spin_0.25s_linear_infinite] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-  class:small
+  class="block h-4 w-4 animate-[spin_0.25s_linear_infinite] motion-reduce:animate-[spin_1.5s_linear_infinite]"
   role="status">
-  <!-- TODO: Add spinner -->
-  Spinner
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+    <path
+      fill="currentColor"
+      d="M2.501 8a5.5 5.5 0 1 1 5.5 5.5A.75.75 0 0 0 8 15a7 7 0 1 0-7-7a.75.75 0 0 0 1.501 0" /></svg>
 </div>
 
 <style lang="postcss">
   @reference "tailwindcss";
-
-  div.small {
-    @apply h-3 w-3;
-  }
 </style>
